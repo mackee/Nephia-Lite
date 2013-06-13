8e5be1d62abdc6786e0fe3b5c16fec62a2fa7040
mackee <macopy123[attttt]gmai.com>
2013-06-13 11:13:03 +0900

    work on 5.12

--- lib/Nephia/Lite.pm
+++ lib/Nephia/Lite.pm
@@ -43,99 +43,98 @@ sub to_app(&@) {
     return $app;
 }
 
-package Nephia::Lite::View {
-    use Nephia::ClassLoader;
+package Nephia::Lite::View;
 
-    sub new {
-        my ($class, %opts) = @_;
-        my $subclass = 'Nephia::Lite::View::MicroTemplate';
-        if (exists $opts{class}) {
-            $subclass = join '::', 'Nephia::View', delete $opts{class};
-            Nephia::ClassLoader->load($subclass);
-        }
-        return $subclass->new(%opts);
+use Nephia::ClassLoader;
+
+sub new {
+    my ($class, %opts) = @_;
+    my $subclass = 'Nephia::Lite::View::MicroTemplate';
+    if (exists $opts{class}) {
+        $subclass = join '::', 'Nephia::View', delete $opts{class};
+        Nephia::ClassLoader->load($subclass);
     }
+    return $subclass->new(%opts);
 }
 
-package Nephia::Lite::View::MicroTemplate {
+package Nephia::Lite::View::MicroTemplate;
 
-    sub new {
-        my ($class, %opts) = @_;
-        my $mt = Text::MicroTemplate::DataSection::ForNephia->new(%opts);
-        bless { mt => $mt }, $class;
-    }
-
-    sub render {
-        my ($self, @params) = @_;
-        $self->{mt}->render_file(@params);
-    }
+sub new {
+    my ($class, %opts) = @_;
+    my $mt = Text::MicroTemplate::DataSection::ForNephia->new(%opts);
+    bless { mt => $mt }, $class;
 }
 
-package Text::MicroTemplate::DataSection::ForNephia {
-    use parent qw/Text::MicroTemplate::File/;
-    use Encode;
-    use Carp;
+sub render {
+    my ($self, @params) = @_;
+    $self->{mt}->render_file(@params);
+}
 
-    sub new {
-        my $self = shift->SUPER::new(@_);
-        $self->{package} ||= scalar caller;
-        $self->{template} = $self->_read_section_data();
+package Text::MicroTemplate::DataSection::ForNephia;
 
-        $self;
+use parent qw/Text::MicroTemplate::File/;
+use Encode;
+use Carp;
+
+sub new {
+    my $self = shift->SUPER::new(@_);
+    $self->{package} ||= scalar caller;
+    $self->{template} = $self->_read_section_data();
+
+    $self;
+}
+
+sub _read_section_data {
+    my $self = shift;
+    my $content;
+    {
+        no strict 'refs';
+        my $d = \*{$self->{package}.'::DATA'};
+        $content = join '', <$d>;
+        $content =~ s/^.*\n__DATA__\n/\n/s;
+        $content =~ s/__END__\n.*$/\n/s;
+    }
+    return $content;
+}
+
+
+sub build_file {
+    my $self = shift;
+
+    if (my $e = $self->{cache}->{'DATA'}) {
+        return $e;
     }
 
-    sub _read_section_data {
-        my $self = shift;
-        my $content;
-        {
-            no strict 'refs';
-            my $d = \*{$self->{package}.'::DATA'};
-            $content = join '', <$d>;
-            $content =~ s/^.*\n__DATA__\n/\n/s;
-            $content =~ s/__END__\n.*$/\n/s;
-        }
-        return $content;
-    }
+    if (my $data = $self->{template}) {
+        my @splited_data = split /\$/, $data;
 
-
-    sub build_file {
-        my $self = shift;
-
-        #if (my $e = $self->{cache}) {
-        #    return $e;
-        #}
-
-        if (my $data = $self->{template}) {
-            my @splited_data = split /\$/, $data;
-
-            shift @splited_data;
-            my @already_vars;
-            for my $segment (@splited_data) {
-                next if $segment =~ /_/;
-                my @words = split /\ /, $segment;
-                my $var_name = shift @words;
-                next if grep { $var_name eq $_ } @already_vars;
-                push @already_vars, $var_name;
-                $data = "? my \$$var_name = \$_[0]->{$var_name};\n".$data;
-            }
-
-            $self->parse(decode_utf8 $data);
-
-            local $Text::MicroTemplate::_mt_setter = 'my $_mt = shift;';
-            my $f = $self->build();
-
-            $self->{cache} = $f if $self->{use_cache};
-            return $f;
+        shift @splited_data;
+        my @already_vars;
+        for my $segment (@splited_data) {
+            next if $segment =~ /_/;
+            my @words = split /\ /, $segment;
+            my $var_name = shift @words;
+            next if grep { $var_name eq $_ } @already_vars;
+            push @already_vars, $var_name;
+            $data = "? my \$$var_name = \$_[0]->{$var_name};\n".$data;
         }
 
-        croak "could not find template content in __DATA__ section";
+        $self->parse(decode_utf8 $data);
+
+        local $Text::MicroTemplate::_mt_setter = 'my $_mt = shift;';
+        my $f = $self->build();
+
+        $self->{cache}->{'DATA'} = $f if $self->{use_cache};
+        return $f;
     }
 
-    sub render_mt {
-        my $self = __PACKAGE__->new(package => shift);
+    croak "could not find template content in __DATA__ section";
+}
 
-        $self->render_file(@_);
-    }
+sub render_mt {
+    my $self = __PACKAGE__->new(package => shift);
+
+    $self->render_file(@_);
 }
 
 1;
